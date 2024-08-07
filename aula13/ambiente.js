59b944e7fa5e4fadba8695d5d3f2e36653210eb2
Davi Lima <dlima12435@gmail.com>
2024-08-06 22:59:57 -0300

    alterações

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,9 +1,5 @@
-for (var c = 1; c <= 3; c++){
-    console.log(`Passo ${c}`)
-}
-/*var c = 1
+var c = 1
 while (c <= 10){
     console.log(`Passo ${c}`)
     c++
 }
-*/
---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,8 @@
+function contagem(){
+
+}
+
+var i = Number.document.getElementById ('inicio')
+var f = Number.document.getElementById ('fim')
+var res = i+f
+res.innerTex = console.log(`O resultado é ${res}`)
